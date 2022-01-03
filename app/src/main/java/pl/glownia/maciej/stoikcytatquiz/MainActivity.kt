3ae648f12bc2713c8e67aee93def4890afe24cbd
Maciej Głownia <maciej.k.glownia@gmail.com>
2022-01-03 13:18:34 +0100

    Make next_button working

--- app/src/main/java/pl/glownia/maciej/stoikcytatquiz/MainActivity.kt
+++ app/src/main/java/pl/glownia/maciej/stoikcytatquiz/MainActivity.kt
@@ -1,11 +1,19 @@
 package pl.glownia.maciej.stoikcytatquiz
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonStart : Button = findViewById(R.id.btnStart)
+        buttonStart.setOnClickListener{
+            val intent = Intent(this@MainActivity, QuizQuestionsActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
