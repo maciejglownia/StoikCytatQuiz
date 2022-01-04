b595ae6c7e8bcb303122cf0d6b8caa97be4be3ff
Maciej Głownia <maciej.k.glownia@gmail.com>
2022-01-04 01:06:10 +0100

    Cosmetic changes - nothing to check

--- app/src/main/java/pl/glownia/maciej/stoikcytatquiz/Constants.kt
+++ app/src/main/java/pl/glownia/maciej/stoikcytatquiz/Constants.kt
@@ -7,9 +7,9 @@ object Constants {
         val question1 = Question(
             1,
             "Czyje to słowa?",
-            "Wiele jest ziarenek kadzielnych na jednym ołtarzu."
+            "''Wiele jest ziarenek kadzielnych na jednym ołtarzu."
                     + " Jedne najpierw wpadają, inne później. " +
-                    "A to nie stanowi różnicy.",
+                    "A to nie stanowi różnicy.''",
             "Epiktet",
             "Marek Aureliusz",
             "Seneka",
@@ -21,7 +21,7 @@ object Constants {
         val question2 = Question(
             2,
             "Czyje to słowa?",
-            "Nie same zdarzenia budzą w ludziach niepokój, ale sądy o nich.",
+            "''Nie same zdarzenia budzą w ludziach niepokój, ale sądy o nich.''",
             "Epiktet",
             "Marek Aureliusz",
             "Seneka",
@@ -33,7 +33,7 @@ object Constants {
         val question3 = Question(
             3,
             "Czyje to słowa?",
-            "Lepiej jest sporządzić bilans dotyczący własnego życia, niż rynku zbóż.",
+            "''Lepiej jest sporządzić bilans dotyczący własnego życia, niż rynku zbóż.''",
             "Epiktet",
             "Marek Aureliusz",
             "Seneka",
@@ -45,9 +45,9 @@ object Constants {
         val question4 = Question(
             4,
             "Czyje to słowa?",
-            "Uważam cię za pechowca, ponieważ nigdy nie zmierzyłes się " +
+            "''Uważam cię za pechowca, ponieważ nigdy nie zmierzyłes się " +
                     "z przeciwnościami losu. Przeszedłeś przez życie bez przeciwnika." +
-                    "Nikt, nawet ty, nie wie do czego jesteś zdolny.",
+                    "Nikt, nawet ty, nie wie do czego jesteś zdolny.''",
             "Epiktet",
             "Marek Aureliusz",
             "Seneka",
@@ -59,8 +59,8 @@ object Constants {
         val question5 = Question(
             5,
             "Czyje to słowa?",
-            "Został osadzony w więzieniu. Jednak spostrzeżenie " +
-                    "''spotkało go zło'' dodałeś już sam.",
+            "''Został osadzony w więzieniu. Jednak spostrzeżenie " +
+                    "> spotkało go zło < dodałeś już sam.''",
             "Epiktet",
             "Marek Aureliusz",
             "Seneka",
@@ -72,7 +72,7 @@ object Constants {
         val question6 = Question(
             6,
             "Czyje to słowa?",
-            "Mamy dwoje uszu i jedne usta, abyśmy słuchali dwa razy tyle, co mówili.",
+            "''Mamy dwoje uszu i jedne usta, abyśmy słuchali dwa razy tyle, co mówili.''",
             "Epiktet",
             "Marek Aureliusz",
             "Seneka",
@@ -84,7 +84,7 @@ object Constants {
         val question7 = Question(
             7,
             "Czyje to słowa?",
-            "Skutki gniewu są dużo poważniejsze od jego przyczyny.",
+            "''Skutki gniewu są dużo poważniejsze od jego przyczyny.''",
             "Epiktet",
             "Marek Aureliusz",
             "Seneka",
@@ -96,7 +96,8 @@ object Constants {
         val question8 = Question(
             8,
             "Czyje to słowa?",
-            "Fortuna pragnie abym miał więcej wolności by filozofować.",
+            "''Fortuna pragnie abym miał więcej wolności by filozofować. " +
+                    "(Na wieść o zatonięciu statku z prawie całym majątkiem)''",
             "Epiktet",
             "Marek Aureliusz",
             "Seneka",
@@ -109,7 +110,7 @@ object Constants {
         val question9 = Question(
             9,
             "Czyje to słowa?",
-            "Chętnego Losy prowadzą, niechętnego wloką.",
+            "''Chętnego Losy prowadzą, niechętnego wloką.''",
             "Epiktet",
             "Marek Aureliusz",
             "Seneka",
@@ -122,7 +123,7 @@ object Constants {
         val question10 = Question(
             10,
             "Czyje to słowa?",
-            "Lekarstwem na przyzwyczajenie jest inne przyzwyczajenie",
+            "''Lekarstwem na przyzwyczajenie jest inne przyzwyczajenie''",
             "Epiktet",
             "Marek Aureliusz",
             "Seneka",
