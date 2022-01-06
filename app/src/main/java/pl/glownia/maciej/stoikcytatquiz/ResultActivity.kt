a36bddbe21587ee679a9991291a67a9f20a8b7b2
Maciej Głownia <maciej.k.glownia@gmail.com>
2022-01-06 08:28:42 +0100

    Create ResultActivity to display last activity

---
+++ app/src/main/java/pl/glownia/maciej/stoikcytatquiz/ResultActivity.kt
@@ -0,0 +1,28 @@
+package pl.glownia.maciej.stoikcytatquiz
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+
+        val tvScore: TextView = findViewById(R.id.tv_score)
+        val btnFinish: Button = findViewById(R.id.btn_finish)
+
+        // intExtra cause mCorrectAnswers in QuizQuestionsActivity is Int
+        val totalQuestions = intent.getIntExtra(Constants.TOTAL_QUESTIONS, 0)
+        val correctAnswers = intent.getIntExtra(Constants.CORRECT_ANSWERS, 0)
+
+        tvScore.text = "Twój wynik to $correctAnswers poprawnych odpowiedzi " +
+                "na $totalQuestions możliwych"
+
+        btnFinish.setOnClickListener {
+            startActivity(Intent(this@ResultActivity, MainActivity::class.java))
+        }
+    }
+}
