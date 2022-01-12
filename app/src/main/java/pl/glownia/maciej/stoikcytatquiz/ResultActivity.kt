a8fe87ba095e5baee3ec152e1f2672f5242a224d
Maciej Głownia <maciej.k.glownia@gmail.com>
2022-01-12 11:27:11 +0100

    Add if statement to obtain correct version of polish words depend on amount of correct and total answers

--- app/src/main/java/pl/glownia/maciej/stoikcytatquiz/ResultActivity.kt
+++ app/src/main/java/pl/glownia/maciej/stoikcytatquiz/ResultActivity.kt
@@ -18,8 +18,34 @@ class ResultActivity : AppCompatActivity() {
         val totalQuestions = intent.getIntExtra(Constants.TOTAL_QUESTIONS, 0)
         val correctAnswers = intent.getIntExtra(Constants.CORRECT_ANSWERS, 0)
 
-        tvScore.text = "Twój wynik to $correctAnswers poprawnych odpowiedzi " +
-                "na \n$totalQuestions możliwych"
+        // Need to take care of endings in polish words depends on amount of correct and total answers
+        var resultCorrectAnswersWordInPolish = "poprawna"
+        var resultTotalAnswersWordInPolish = "możliwą"
+        var answerWordFormInPolish = "odpowiedź"
+
+        if (correctAnswers in 2..4) {
+            resultCorrectAnswersWordInPolish = "poprawne"
+        } else if (correctAnswers == 0 || correctAnswers in 5..100) {
+            resultCorrectAnswersWordInPolish = "poprawnych"
+        }
+
+        if (correctAnswers > 1) {
+            answerWordFormInPolish = "odpowiedzi"
+        }
+        if (totalQuestions == 1) {
+            resultTotalAnswersWordInPolish = "możliwą"
+        } else if (totalQuestions in 1..4) {
+            resultTotalAnswersWordInPolish = "możliwe"
+        } else if (totalQuestions == 0 || totalQuestions > 4) {
+            resultTotalAnswersWordInPolish = "możliwych"
+        }
+
+        // Use resource string with placeholders
+        // in other way there will be warning: do not concatenate text displayed with setText
+        val resultText = "$correctAnswers $resultCorrectAnswersWordInPolish " +
+                "$answerWordFormInPolish na $totalQuestions $resultTotalAnswersWordInPolish"
+
+        tvScore.text = resultText
 
         btnFinish.setOnClickListener {
             startActivity(Intent(this@ResultActivity, MainActivity::class.java))
