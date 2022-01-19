5d9df495bf77269416d9a849fbbfaaa8d8e5c55b
Maciej Głownia <maciej.k.glownia@gmail.com>
2022-01-19 08:48:11 +0100

    Change intent functionality to let user go to main menu where they can choose how many questions they want instead to display questions

--- app/src/main/java/pl/glownia/maciej/stoikcytatquiz/MainActivity.kt
+++ app/src/main/java/pl/glownia/maciej/stoikcytatquiz/MainActivity.kt
@@ -5,6 +5,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 
+/**
+ * Welcome activity.
+ * Displayed only once at the beginning.
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -12,7 +16,7 @@ class MainActivity : AppCompatActivity() {
 
         val buttonStart : Button = findViewById(R.id.btnStart)
         buttonStart.setOnClickListener{
-            val intent = Intent(this@MainActivity, QuizQuestionsActivity::class.java)
+            val intent = Intent(this@MainActivity, MenuActivity::class.java)
             startActivity(intent)
         }
     }
