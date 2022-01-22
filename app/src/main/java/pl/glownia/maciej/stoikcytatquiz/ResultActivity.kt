5d48fada0afc7d62535b6fc4b95184cd90721937
Maciej Głownia <maciej.k.glownia@gmail.com>
2022-01-22 19:00:55 +0100

    Create methods for if statements to check correct form of Polish words depend on result

--- app/src/main/java/pl/glownia/maciej/stoikcytatquiz/ResultActivity.kt
+++ app/src/main/java/pl/glownia/maciej/stoikcytatquiz/ResultActivity.kt
@@ -1,45 +1,35 @@
 package pl.glownia.maciej.stoikcytatquiz
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class ResultActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
 
         val tvScore: TextView = findViewById(R.id.tv_score)
         val btnFinish: Button = findViewById(R.id.btn_finish)
-
-        // intExtra cause mCorrectAnswers in QuizQuestionsActivity is Int
-        val totalQuestions = intent.getIntExtra(Constants.TOTAL_QUESTIONS, 0)
-        val correctAnswers = intent.getIntExtra(Constants.CORRECT_ANSWERS, 0)
-
         // Need to take care of endings in polish words depends on amount of correct and total answers
         var resultCorrectAnswersWordInPolish = "poprawna"
         var resultTotalAnswersWordInPolish = "możliwą"
         var answerWordFormInPolish = "odpowiedź"
 
-        if (correctAnswers in 2..4) {
-            resultCorrectAnswersWordInPolish = "poprawne"
-        } else if (correctAnswers == 0 || correctAnswers in 5..100) {
-            resultCorrectAnswersWordInPolish = "poprawnych"
-        }
+        // intExtra cause mCorrectAnswers in QuizQuestionsActivity is Int
+        val totalQuestions = intent.getIntExtra(Constants.TOTAL_QUESTIONS, 0)
+        val correctAnswers = intent.getIntExtra(Constants.CORRECT_ANSWERS, 0)
 
-        if (correctAnswers > 1) {
-            answerWordFormInPolish = "odpowiedzi"
-        }
-        if (totalQuestions == 1) {
-            resultTotalAnswersWordInPolish = "możliwą"
-        } else if (totalQuestions in 1..4) {
-            resultTotalAnswersWordInPolish = "możliwe"
-        } else if (totalQuestions == 0 || totalQuestions > 4) {
-            resultTotalAnswersWordInPolish = "możliwych"
-        }
-
+        // Different endings in Polish depends on correctAnswers and/or totalQuestions
+        resultCorrectAnswersWordInPolish =
+            checkCorrectFormOfCorrectWord(correctAnswers, resultCorrectAnswersWordInPolish)
+        answerWordFormInPolish =
+            checkCorrectFormOfAnswerWord(correctAnswers, answerWordFormInPolish)
+        resultTotalAnswersWordInPolish =
+            checkCorrectFormOfPossibleWord(totalQuestions, resultTotalAnswersWordInPolish)
         // Use resource string with placeholders
         // in other way there will be warning: do not concatenate text displayed with setText
         val resultText = "$correctAnswers $resultCorrectAnswersWordInPolish " +
@@ -50,5 +40,44 @@ class ResultActivity : AppCompatActivity() {
         btnFinish.setOnClickListener {
             startActivity(Intent(this@ResultActivity, MenuActivity::class.java))
         }
+    }
+
+    private fun checkCorrectFormOfPossibleWord(
+        totalQuestions: Int,
+        resultTotalAnswersWordInPolish: String
+    ): String {
+        var resultTotalAnswersWordInPolish1 = resultTotalAnswersWordInPolish
+        if (totalQuestions == 1) {
+            resultTotalAnswersWordInPolish1 = "możliwą"
+        } else if (totalQuestions in 1..4) {
+            resultTotalAnswersWordInPolish1 = "możliwe"
+        } else if (totalQuestions == 0 || totalQuestions > 4) {
+            resultTotalAnswersWordInPolish1 = "możliwych"
+        }
+        return resultTotalAnswersWordInPolish1
+    }
+
+    private fun checkCorrectFormOfAnswerWord(
+        correctAnswers: Int,
+        answerWordFormInPolish: String
+    ): String {
+        var answerWordFormInPolish1 = answerWordFormInPolish
+        if (correctAnswers > 1) {
+            answerWordFormInPolish1 = "odpowiedzi"
+        }
+        return answerWordFormInPolish1
+    }
+
+    private fun checkCorrectFormOfCorrectWord(
+        correctAnswers: Int,
+        resultCorrectAnswersWordInPolish: String
+    ): String {
+        var resultCorrectAnswersWordInPolish1 = resultCorrectAnswersWordInPolish
+        if (correctAnswers in 2..4) {
+            resultCorrectAnswersWordInPolish1 = "poprawne"
+        } else if (correctAnswers == 0 || correctAnswers in 5..100) {
+            resultCorrectAnswersWordInPolish1 = "poprawnych"
+        }
+        return resultCorrectAnswersWordInPolish1
     }
 }
