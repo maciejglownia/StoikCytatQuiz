702ae0f3aedfdb93a2789f6ca095a071f04d421e
Maciej Głownia <maciej.k.glownia@gmail.com>
2022-01-23 15:32:48 +0100

    Set up sending email to developer

--- app/src/main/java/pl/glownia/maciej/stoikcytatquiz/SettingsActivity.kt
+++ app/src/main/java/pl/glownia/maciej/stoikcytatquiz/SettingsActivity.kt
@@ -1,11 +1,23 @@
 package pl.glownia.maciej.stoikcytatquiz
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        // Set up sending mail to developer
+        val contact: LinearLayout = findViewById(R.id.ll_contact)
+        contact.setOnClickListener {
+            val emailIntent = Intent(Intent.ACTION_SENDTO).apply {
+                data = Uri.parse("mailto:maciej.k.glownia@gmail.com")
+            }
+            startActivity(Intent.createChooser(emailIntent, "Send feedback"))
+        }
     }
 }
